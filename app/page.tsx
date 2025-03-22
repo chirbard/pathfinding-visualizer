bc278596250b21502e54599efaa6bb9fcdf3c231
chirbard <matamm@outlook.com>
2025-03-22 16:46:27 +0200

    feat: add source

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+"use client"
+
+import PathfindingVisualization from "../pathfinding-visualization"
+
+export default function SyntheticV0PageForDeployment() {
+  return <PathfindingVisualization />
+}
